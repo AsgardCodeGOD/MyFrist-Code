3ef6f1c7f6186ec91a04316919230f246cde29ce
AsgardCodeGOD <40168363+AsgardCodeGOD@users.noreply.github.com>
2018-06-11 14:30:38 +0200

    Revert "Revert "My First Variable""
    This reverts commit 572d939f95a75009026081065daff2b2d9c731f4.

---
+++ First Variable Code/First Variable Code/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Variable_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("My variable value = {0}",x);
+            Console.ReadKey();
+        }
+    }
+}
